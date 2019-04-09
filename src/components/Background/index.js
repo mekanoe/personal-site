bc31fbaaf6b5769a3fe5eb81d52217ab0c1ff2f4
Kata <git@kat.cafe>
2019-04-09 16:08:06 -0500

    remove old files after rebase

--- src/components/Background/index.js
+++
@@ -1,8 +0,0 @@
-import './Background.css'
-export default ({ready}) => {
-  return (<div>
-    <div className={'Background Background-ready_' + ready} />
-    <div className='Background' style={{zIndex: -101}} />
-  </div>
-  )
-}
